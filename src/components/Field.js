9fb4867089c215781515ff9c0e4b958e47222310
Taras Mazurkevych <mad.taras@gmail.com>
2017-05-07 18:15:55 +0300

    Rewrite field styles from flexbox to float for better performance

--- src/components/Field.js
+++ src/components/Field.js
@@ -5,11 +5,11 @@ class Field extends Component {
   render() {
     const { field } = this.props;
     const fieldItems = field.map((col) => {
-      return col.map((item) => <div style={{height: 3, width: 3, backgroundColor: item ? '#212121' : '#fff'}}> </div>)
+      return col.map((item) => <div style={{height: 3, width: 3, float: 'left', backgroundColor: item ? '#212121' : '#fff'}}> </div>)
     });
 
     return (
-      <div style={{height: 600, width: 600, display: 'flex', flexWrap: 'wrap'}}>
+      <div style={{height: 600, width: 600}}>
         {fieldItems}
       </div>
     )
